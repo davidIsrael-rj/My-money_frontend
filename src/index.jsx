ee06f143affa49a3f8eb3ac8cbff5167d44ce921
DavidIsrael <davidisrael.rj@gmail.com>
2024-07-07 07:16:53 -0300

    refatorando as dependências para poder acompanhar o curso, instalando uma versão mais antiga do react (15.4.2), dependencies e custom

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from 'react-dom'
-import registerServiceWorker from './registerServiceWorker'
 import App from "./main/app";
 
 ReactDOM.render(<App />, document.getElementById('app'))
-registerServiceWorker()
