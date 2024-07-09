99467fe0cd43327804fd9d362d33b632ae36d6ed
DavidIsrael <davidisrael.rj@gmail.com>
2024-07-09 14:08:20 -0300

    Navegação com React-router

---
+++ src/main/routes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Router, Redirect, hashHistory } from 'react-router'
+
+import Dashboard from "../dashboard/dashboard";
+import BillingCycle from "../billingCycle/billingCycle";
+
+export default props => (
+    <Router history={hashHistory}>
+        <Router path='/' component={Dashboard}/>
+        <Route path='/billingCycles' component={BillingCycle}/>
+        <Redirect from='*' to='/'/>
+    </Router>
+)
