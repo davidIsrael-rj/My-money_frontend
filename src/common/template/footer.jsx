97ca8fc92e0e61b44c2692ee358f00174f4a5766
DavidIsrael <davidisrael.rj@gmail.com>
2024-07-08 22:47:15 -0300

    Componente Rodapé

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2024
+            <a href="#" target="_blank">David</a>
+        </strong>
+    </footer>
+)
