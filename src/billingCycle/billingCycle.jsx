a029ff8b4f549bbacb07bed6f44942af25653d4c
DavidIsrael <davidisrael.rj@gmail.com>
2024-07-15 09:41:46 -0300

    Adicionando o componente TabContent ao billingCycle

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -6,22 +6,26 @@ import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabHeader from "../common/tab/tabHeader";
 import TabsContent from "../common/tab/tabsContent";
+import TabContent from "../common/tab/tabContent";
 
 class BillingCycle extends Component {
-    render(){
+    render() {
         return (
             <div>
-                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro'/>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
                 <Content>
                     <Tabs>
                         <TabsHeader>
-                            <TabHeader label='Listar' icon='bars' target='tabList'/>
-                            <TabHeader label='Incluir' icon='plus' target='tabCreate'/>
-                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate'/>
-                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-                            
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
