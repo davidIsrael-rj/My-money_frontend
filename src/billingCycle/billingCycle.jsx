0f908b5c659695acd30fb732e2296086923b13b4
DavidIsrael <davidisrael.rj@gmail.com>
2024-07-16 21:38:35 -0300

    Visibilidade das Abas (Parte 1)

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -9,12 +9,13 @@ import TabsHeader from "../common/tab/tabsHeader";
 import TabHeader from "../common/tab/tabHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabContent from "../common/tab/tabContent";
-import { selectTab } from "../common/tab/tabAction";
+import { selectTab, showTabs } from "../common/tab/tabAction";
 
 class BillingCycle extends Component {
 
     componentWillMount(){
         this.props.selectTab('tabList')
+        this.props.showTabs('tabList', 'tabCreate')
     }
     render() {
         return (
@@ -41,5 +42,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
