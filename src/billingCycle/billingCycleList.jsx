0176c7e1f4808967d732aa1c875d08160eb46379
DavidIsrael <davidisrael.rj@gmail.com>
2024-07-20 22:20:26 -0300

    Exibindo os dados em BillingCycleList

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -5,11 +5,23 @@ import { getList } from "./billingCycleAction";
 
 
 class BillingCycleList extends Component {
-    componentWillMount(){
+    componentWillMount() {
         this.props.getList()
     }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>                
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
+    }
+
     render() {
-        console.log(this.props.list)
+        // console.log(this.props.list)
         return (
             <div>
                 <table className="table">
@@ -21,7 +33,7 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
@@ -29,6 +41,6 @@ class BillingCycleList extends Component {
     }
 }
 
-const mapStateToProps = state =>({list: state.billingCycle.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+const mapStateToProps = state => ({ list: state.billingCycle.list })
+const mapDispatchToProps = dispatch => bindActionCreators({ getList }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
