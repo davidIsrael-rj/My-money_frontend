b3c89d67a1b6407c2d2ea589f4475b56c6df013c
DavidIsrael <davidisrael.rj@gmail.com>
2024-07-24 18:37:13 -0300

    Melhorias na Inclusão do Ciclo de Pagamento

--- src/billingCycle/billingCycleAction.js
+++ src/billingCycle/billingCycleAction.js
@@ -1,25 +1,34 @@
 import axios from "axios";
 import { toastr } from "react-redux-toastr";
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from "../common/tab/tabAction";
+
+
 const BASE_URL = 'http://localhost:3003/api'
 
-export function getList(){
+export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
-    return{
+    return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
     }
 }
 
-export function create(values){
-    axios.post(`${BASE_URL}/billingCycles`, values)
-        .then(resp => {
-            toastr.success('Sucesso', 'Operação Realizada com sucesso.')
-        })
-        .catch(e => {
-            e.response.data.errors.forEach(error => toastr.error('Erro', error))
-        })
-    return{
-        type: 'TEMP'
-        
+export function create(values) {
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate')
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
     }
+
 }
