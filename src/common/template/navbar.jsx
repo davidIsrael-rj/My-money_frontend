d07e1c3ec0a4fceaf3ec3213cb0c070776dda7f1
DavidIsrael <davidisrael.rj@gmail.com>
2024-08-13 19:48:03 -0300

    Impelmentando a estrutura do navbar

--- src/common/template/navbar.jsx
+++ src/common/template/navbar.jsx
@@ -16,7 +16,34 @@ class Navbar extends Component {
     render() {
         const {name, email} = this.props.user 
         return (
-            <div></div>
+            <div className="navbar-custom-menu">
+                <ul className="nav navbar-nav">
+                    <li onMouseLeave={() => this.changeOpen()}
+                        className={`dropdown user-menu ${this.state.open ? 'open' : ''}`}>
+                            <a href="javascript:;" onClick={() => this.changeOpen()}
+                                aria-expanded={this.state.open ? 'thue' : 'false'}
+                                className="dropdown-toggle"
+                                data-toggle="dropdown">
+                                    <img src="http://lorempixel.com/160/160/abstract" 
+                                        className="user-image" alt="User Image" />
+                                        <span className="hidden-xs">{name}</span>
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li className="user-header">
+                                        <img src="http://lorempixel.com/160/160/abstract"
+                                            className="img-circle" alt="User Image" />
+                                            <p>{name} <small>{email}</small></p>
+                                    </li>
+                                    <li className="user-footer">
+                                        <div className="pull-right">
+                                            <a href="#" onClick={this.props.logout}
+                                            className="btn btn-default btn-flat">Sair</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                        </li>
+                </ul>
+            </div>
         )
     }
 }
